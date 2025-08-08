413919a8dd9a7df86eb209931feea30fa9fcc86e
Cypher <decypher@skiff.com>
2025-08-08 23:45:44 +0200

    casing

--- app/constants/contracts.ts
+++ app/constants/contracts.ts
@@ -1,7 +1,7 @@
 // GMNFT Contract Address
 // This contract is deployed on Hedera Testnet (Chain ID: 296)
 // Contract includes functions: canMint(address) and mint()
-export const GMNFT_ADDRESS = '0xF7FDCCDBB32DbecCBaBC5f346AB67Ce1C892012f';
+export const GMNFT_ADDRESS = '0xF7FDCCDBB32DbEcCBaBC5f346AB67Ce1C892012f';
 
 // CREDIT Token Contract Address
 // This contract is deployed on Hedera Testnet (Chain ID: 296)
@@ -15,7 +15,7 @@ export const WATER_ADDRESS = '0xa21FCeF30b868D5825B2506253eF2afc9AC80FCD';
 
 // Deployment Summary:
 // - CREDIT: 0xDdDF82a67A934cE809a754783affE9f35c0D0545
-// - GMNFT: 0xF7FDCCDBB32DbecCBaBC5f346AB67Ce1C892012f
+// - GMNFT: 0xF7FDCCDBB32DbEcCBaBC5f346AB67Ce1C892012f
 // - WATER: 0xa21FCeF30b868D5825B2506253eF2afc9AC80FCD
 // - RPC: https://testnet.hashio.io/api
 // - Gas Limit: 60,000,000
